03a4e106036994bd7892839c81c2e09144979719
Tim Simpson <timsimpson4@gmail.com>
2019-12-23 22:44:36 -0600

    makes CLI less terrible

--- mrst/cli.py
+++ mrst/cli.py
@@ -9,24 +9,24 @@ from . import gen
 def run(args: t.List[str]) -> int:
     parser = argparse.ArgumentParser("Generates Rst files")
     parser.add_argument(
-        "--source", default=None, type=str, help="source directory"
+        "--source", required=True, type=str, help="source directory"
     )
     parser.add_argument(
         "--output",
-        default=None,
         type=str,
+        required=True,
         help="destination directory for generated source",
     )
     parser.add_argument(
-        "--generate",
+        "--skip-sphinx",
+        action="store_true",
         default=False,
-        type=bool,
         help="If set, generate only, don't call Sphinx.",
     )
     p_args = parser.parse_args(args)
 
     cfg = gen.Config(p_args.source, p_args.output)
-    if p_args.generate:
+    if p_args.no_sphinx:
         return gen.generate(cfg)
     else:
         return build.build(cfg)
